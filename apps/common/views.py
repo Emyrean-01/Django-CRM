5bd4fe527b7858a617469630fed6851860472aed
akashpatil1997 <patilakash2468@gmail.com>
2020-06-22 13:04:52 +0530

    My very first commit

---
+++ apps/common/views.py
@@ -0,0 +1,135 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from .forms import SignUpForm, UserForm, ProfileForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+#  django.contrib import messages
+from apps.userprofile.models import Profile
+from django.views.generic import ListView
+from .models import CrudUser
+
+
+class CrudView(ListView):
+    model = CrudUser
+    template_name = 'common/crud.html'
+    context_object_name = 'users'
+
+from .models import CrudUser
+from django.views.generic import View
+from django.http import JsonResponse
+
+# CRUD Create User
+class CreateCrudUser(View):
+    def  get(self, request):
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.create(
+            name = name1,
+            address = address1,
+            age = age1
+        )
+
+        user = {'id':obj.id,'name':obj.name,'address':obj.address,'age':obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)
+
+# CRUD update user
+class UpdateCrudUser(View):
+    def  get(self, request):
+        id1 = request.GET.get('id', None)
+        name1 = request.GET.get('name', None)
+        address1 = request.GET.get('address', None)
+        age1 = request.GET.get('age', None)
+
+        obj = CrudUser.objects.get(id=id1)
+        obj.name = name1
+        obj.address = address1
+        obj.age = age1
+        obj.save()
+
+        user = {'id':obj.id,'name':obj.name,'address':obj.address,'age':obj.age}
+
+        data = {
+            'user': user
+        }
+        return JsonResponse(data)
+#CRUD Delete User
+class DeleteCrudUser(View):
+    def  get(self, request):
+        id1 = request.GET.get('id', None)
+        CrudUser.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)        
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'common/dashboard.html'
+    login_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        print(self.request.user.id)
+        context['book_list'] = self.request.user
+        return context
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')
+    template_name = 'common/register.html'
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import UserForm, ProfileForm
+from django.contrib.auth.models import User
+from apps.userprofile.models import Profile
+from django.contrib import messages
+
+# from django.contrib import messages
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'common/profile.html'
+
+class ProfileUpdateView(LoginRequiredMixin, TemplateView):
+    user_form = UserForm
+    profile_form = ProfileForm
+    template_name = 'common/profile-update.html'
+
+    def post(self, request):
+
+        post_data = request.POST or None
+        file_data = request.FILES or None
+
+        user_form = UserForm(post_data, instance=request.user)
+        profile_form = ProfileForm(post_data, file_data, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.error(request, 'Your profile is updated successfully!')
+            return HttpResponseRedirect(reverse_lazy('profile'))
+
+        context = self.get_context_data(
+                                        user_form=user_form,
+                                        profile_form=profile_form
+                                    )
+
+        return self.render_to_response(context)     
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    
